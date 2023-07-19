1ad3673c6acbfe679dcc5a47aa50a12ce4d06ebc
Mudra Verma <md393822@dal.ca>
2023-07-19 15:06:49 -0300

    PART 3 Lambda

---
+++ LamdaFunctions/upload_files_to_s3.py
@@ -0,0 +1,23 @@
+import os
+import time
+import boto3
+
+def upload_files_to_s3(event, context):
+    s3_client = boto3.client('s3')
+    folder_path = 'C:/CSCI5410/A3/t'  # Update with the actual folder path on your machine
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            time.sleep(0.1)  # Delay of 100 milliseconds
+
+            try:
+                s3_client.upload_file(file_path, 'sampledata-b00932103', filename)
+                print(f"Uploaded {filename} to S3 bucket")
+            except Exception as e:
+                print(f"Failed to upload {filename}: {str(e)}")
+
+    return 'Upload completed'
+
+# Testing the function locally
+upload_files_to_s3(None, None)
